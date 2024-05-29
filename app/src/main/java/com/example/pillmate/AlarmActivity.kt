105c83a3661e90b851c61a2df4bf871bac486b75
gahyunkim <mapledt001@naver.com>
2024-05-29 23:25:03 +0900

    Feat: AlarmActivity 코드 및 디자인 구현(#9)
    - 수정된 화면 디자인으로 액티비티 화면 구현 완료
    - 화면 내에 들어가는 시간, 날짜 등 코드로 설정 완료
    - 알람 사용을 위한 manifest 설정 추가
    
    Related to: #9

--- app/src/main/java/com/example/pillmate/AlarmActivity.kt
+++ app/src/main/java/com/example/pillmate/AlarmActivity.kt
@@ -1,20 +1,108 @@
 package com.example.pillmate
 
+import android.annotation.SuppressLint
+import android.app.KeyguardManager
+import android.content.Context
+import android.graphics.Paint
 import android.os.Bundle
+import android.os.PowerManager
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.pillmate.databinding.ActivityAlarmBinding
+import com.example.pillmate.databinding.ActivityAlarmListBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class AlarmActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAlarmBinding
+    private lateinit var calendar: Calendar
+    private lateinit var powerManager: PowerManager
+    private var flag = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_alarm)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding = ActivityAlarmBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // xml에서 텍스트 밑줄
+        binding.todayNoneBtn.setPaintFlags(Paint.UNDERLINE_TEXT_FLAG)
+
+        calendar = Calendar.getInstance()
+
+        powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+
+        turnScreenOnAndKeyguardOff()
+
+        Thread {
+            while (flag) {
+                try {
+                    calendar = Calendar.getInstance()
+                    runOnUiThread {
+                        updateTimeText()
+                    }
+                    Thread.sleep(1000)
+                } catch (ex: InterruptedException) {
+                    ex.printStackTrace()
+                }
+            }
+        }.start()
+
+        binding.pillBtn.setOnClickListener {
+            flag = false
+            finish()
+        }
+
+    }
+
+    private fun updateTimeText() {
+        val hourOfDay = calendar[Calendar.HOUR_OF_DAY]
+        val minute = calendar[Calendar.MINUTE]
+        val timeString: String
+
+        val formattedMinute = if (minute < 10) {
+            "0$minute"
+        } else {
+            "$minute"
+        }
+
+        if (hourOfDay in 1..11) {
+            timeString = "$hourOfDay:$formattedMinute"
+        } else if (hourOfDay == 12) {
+            timeString = "$hourOfDay:$formattedMinute"
+        } else {
+            val formattedHour = if (hourOfDay > 12) {
+                hourOfDay - 12
+            } else {
+                hourOfDay
+            }
+            timeString = "$formattedHour:$formattedMinute"
+        }
+
+        binding.time.text = timeString
+
+        val dateFormat = SimpleDateFormat("MM월 dd일 EEEE", Locale.getDefault())
+        val dateString = dateFormat.format(calendar.time)
+        binding.date.text = dateString
+    }
+    @SuppressLint("NewApi")
+    private fun turnScreenOnAndKeyguardOff() {
+        window.addFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+        )
+
+        setShowWhenLocked(true)
+        setTurnScreenOn(true)
+
+        // Android 12 이상에서는 KeyguardManager를 사용하여 잠금 화면을 해제합니다.
+        val keyguardManager = getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager
+        if (keyguardManager.isKeyguardLocked) {
+            keyguardManager.requestDismissKeyguard(this, null)
         }
     }
 }
