c2cf4d21145fade64e9c76e5971ffd36ff49bb17
gahyunkim <mapledt001@naver.com>
2024-09-05 12:02:21 +0900

    Feat: API 기본 설정 구현
    - BaseUrl 구성 및 서비스 화면 구현
    - API 연결을 위한 인터넷 연결
    
    Related to: #

---
+++ app/src/main/java/com/example/pillmate/RetrofitApi.kt
@@ -0,0 +1,16 @@
+package com.example.pillmate
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitApi {
+    private const val BASE_URL = "http://localhost:8080/api/v1/"
+    private val getRetrofit by lazy{
+        Retrofit.Builder()
+//            .client(okHttpClient) //토큰 인터셉터
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val getRetrofitService:RetrofitService by lazy{getRetrofit.create(RetrofitService::class.java)}
+}
