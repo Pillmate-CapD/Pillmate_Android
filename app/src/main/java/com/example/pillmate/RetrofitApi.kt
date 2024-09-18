3c689b32ab7d845f1b29b68a749daad47773554f
gahyunkim <mapledt001@naver.com>
2024-09-18 23:22:30 +0900

    BASE_URL 변경

--- app/src/main/java/com/example/pillmate/RetrofitApi.kt
+++ app/src/main/java/com/example/pillmate/RetrofitApi.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitApi {
     private val okHttpClient = OkHttpClient.Builder().addInterceptor(AuthInterceptor()).build()
 
-    private const val BASE_URL = "http://ec2-13-209-62-93.ap-northeast-2.compute.amazonaws.com:8080/api/v1/"
+    private const val BASE_URL = "https://www.pillmate.shop/api/v1/"
     private val getRetrofit by lazy{
         Retrofit.Builder()
             .client(okHttpClient) //토큰 인터셉터
